ff2fba3b0f61090b66114e5528874a6bf1027ad3
Deirdre Bolger <deirdre.bolger76@gmail.com>
2024-02-29 18:18:16 +0100

    Add script to visualize ERPs

--- BrainIHM_ERPviz.py
+++ BrainIHM_ERPviz.py
@@ -6,8 +6,12 @@ import os
 import matplotlib
 from matplotlib.patches import FancyArrowPatch, ArrowStyle, Rectangle
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import matplotlib.image as mpimg
 from matplotlib.backend_bases import MouseButton
 from matplotlib.widgets import SpanSelector
+from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
+from matplotlib.cbook import get_sample_data
 import math
 import pandas as pd
 
@@ -85,15 +89,50 @@ def stimLen_Calc(groupnom, condname, slenDir):
     stimDur_stdLow = stimDur_mean - stimDur_std
     stimDur_stdHigh = stimDur_mean + stimDur_std
 
+    # Plot the mean and std duration for the different feedbacks.
+
     return stimDur_mean, stimDur_stdLow, stimDur_stdHigh
+
+def makeEmptyGraph(T, dataIn, baseD):
+    '''
+    Sub-function to crete an empty plot to include in a top corner of the figure.
+    '''
+    imgNom = 'smallaxes1.png'
+    fig1, ax = plt.subplots(1, 1, figsize=(3.5, 2.5))
+    ax.plot(T, dataIn, 'white', alpha=1)
+    ax.axvline(x=0, c="black")
+    ax.axhline(y=0, c="black")
+    ax.invert_yaxis()
+    ax.set_frame_on(0)
+    ax.set_ylim(bottom=6, top=-6)
+    ax.set_xticks(np.arange(-250, 850, 200))
+
+    ax.tick_params(axis='both', colors='black', labelsize=10)
+    ax.annotate(r"$\mu$V", xy=(-100, -6), xycoords='data', fontsize=10)
+    #ax.set_yticks([-6, 0, 6])
+    ax.annotate("msec", xy=(854, 0.25), xycoords='data', fontsize=10)
+    formatter = mticker.ScalarFormatter(useMathText=True)
+    ax.yaxis.set_major_formatter(formatter)
+    fig1.savefig(os.path.join(baseD, imgNom))
+    plt.close(fig1)
+
+    return imgNom
 
 
 dir_base = '/Users/bolger/Documents/work/Projects/project_Brain-IHM'
 dir_soundLen = os.path.join(dir_base,'SoundFile_Data')
-Groups = ['Agent', 'Agent']
+Groups = ['Human', 'Human']
 
 # Define the video-type and the feedback types.
-Conds2plot = ['InCongru-Congru', 'Congru-Congru']  # Needs to be the same length as groups.
+Conds2plot = ['InCongru-InCongru', 'InCongru-Congru']  # Needs to be the same length as groups.
+
+#Define the textfile with channel-wise statistical results.
+dirMatlab = '/Users/bolger/PycharmProjects/ProjectBrainIHM_EEG'
+statFile = os.path.join(dirMatlab, 'StatsData')
+statFileName = 'Human-Cong-Incong.txt'
+statFullFile = os.path.join(statFile, statFileName)
+statDF = pd.read_csv(statFullFile, sep=",")    # Load in data file as a dataframe
+chanList = list(statDF.columns)
 
 if len(Conds2plot) == 3:
     print(f'Need to collapse conditions: {Conds2plot[0]} and {Conds2plot[1]}')
@@ -129,7 +168,7 @@ for gcount, gcurr in enumerate(Groups):
         condcurrf = 'CongruentAll'
     else:
         condcurrf = gcurr + '-' + condcurr
-    condnames.append(condcurrf)
+    #condnames.append(condcurrf)
     EvokedLoad = {}
     EvokedCSD = {}
     evokeddata_cat = []
@@ -146,11 +185,14 @@ for gcount, gcurr in enumerate(Groups):
             condsplit = condcurr.split('-')
             cond2load = condsplit[len(condsplit) - 1]
             E = mne.read_evokeds(evoked2load_path, condition=None, baseline=None, kind='average')
+
+            # Somewhere here need to keep count of trials per condition.
+
             EvokedLoad[scount] = E[0]
             EvokedLoad[scount].copy().set_eeg_reference(projection=True).apply_proj()
             EvokedCSD[scount] = mne.preprocessing.compute_current_source_density(EvokedLoad[scount])
-            Evoked_data = EvokedLoad[scount].get_data()
-            EvokedCSD_data = EvokedCSD[scount].get_data()
+            Evoked_data = (EvokedLoad[scount].get_data())*(pow(10, 6))
+            EvokedCSD_data = EvokedCSD[scount].get_data()*(pow(10, 6))
 
             if scount == 0:
                 evokeddata_cat = Evoked_data
@@ -170,6 +212,7 @@ for gcount, gcurr in enumerate(Groups):
     if gcount == 0:
         channoms = EvokedLoad[0].info['ch_names']
         times = EvokedLoad[0].times
+        times = times*1000
         EvokedAll_cond = Evokeddata_mean
         EvokedAllCSD_cond = EvokedCSDdata_mean
 
@@ -190,9 +233,31 @@ shape_ev = np.shape(EvokedAll_cond)
 cols = 3
 rows = int(len(roi) / cols)
 fig, axes = plt.subplots(rows, cols, figsize=(30, 10))
+# Add title to the top of the figure.
+if Groups[0]==Groups[1]:
+    G1 = Groups[0]
+    fig.suptitle(G1.upper(), fontweight='bold')
+    c1, c1use = Conds2plot[0].split('-')
+    c2, c2use = Conds2plot[1].split('-')
+    if c1use == 'InCongru':
+        c1use = 'Incongru'
+    elif c2use == 'InCongru':
+        c2use = 'Incongru'
+
+    C1 = c1use+'ous'
+    C2 = c2use+'ous'
+    condnames = [C1, C2]
+elif Conds2plot[0] == Conds2plot[1]:
+    c1, c1use = Conds2plot[0].split('-')
+    if c1use =='InCongru':
+        c1use = 'Incongru'
+    C1 = c1use+'ous'
+    fig.suptitle(C1.upper(), fontweight='bold')
+    condnames = Groups
+
 counter = 0
 colrs = ['r', 'g']
-x0, y0, width, height = -0.5, 1.85, 0, 0   # bbox_to_anchor legend parameters.
+x0, y0, width, height = -0.5, 1.87, 0, 0   # bbox_to_anchor legend parameters.
 
 for axs, ecurr in zip(axes.ravel(), eindx):
 
@@ -205,50 +270,75 @@ for axs, ecurr in zip(axes.ravel(), eindx):
     y1 = EvokedAll_cond[ecurr, :, 0]
     y2 = EvokedAll_cond[ecurr, :, 1]
     x = times
-    axs.plot(times, EvokedAll_cond[ecurr, :, 0], 'darkgray', label=condnames[0])
+    axs.set_axisbelow(True)
+    axs.plot(times, y1, 'darkgray', label=condnames[0])
     axs.plot(times, upper1[ecurr, :], 'darkgray')
     axs.plot(times, lower1[ecurr, :], 'darkgray')
+    axs.grid(axis='both', color='0.95')
     axs.fill_between(times, lower1[ecurr, :], upper1[ecurr, :], facecolor='silver', alpha=0.75, linewidth=0.75)
-    hdlA1 = axs.annotate('', xy=(all_meanSDur[0], -1 * (pow(10, -6))), xytext=(0, -1 * (pow(10, -6))),
-                 arrowprops=dict(facecolor='red', width=2, headwidth=5), label='Mean FB duration(ms): '+condnames[0])
-    styleA1 = ArrowStyle('<->', head_length=2, head_width=1.5)
-    arrow1 = FancyArrowPatch((all_stdSDur_high[0], -1.5 * (pow(10, -6))), (all_stdSDur_low[0], -1.5 * (pow(10, -6))),
-                             arrowstyle=styleA1, color='red', label='Mean and STD of FB length(ms): '+condnames[0])
+    styleA1 = ArrowStyle('simple', head_length=5, head_width=4, tail_width=1)
+    arrow1 = FancyArrowPatch((0, -1.5), (all_meanSDur[0]*1000, -1.5),
+                              arrowstyle=styleA1, color='darkgray', label='Mean feedback duration (msec): '+condnames[0])
     axs.add_patch(arrow1)
 
-    axs.plot(times, EvokedAll_cond[ecurr, :, 1], 'steelblue', label=condnames[1])
-    axs.plot(times, upper2[ecurr, :], 'steelblue')
-    axs.plot(times, lower2[ecurr, :], 'steelblue')
-    axs.fill_between(times, lower2[ecurr, :], upper2[ecurr, :], facecolor='steelblue', alpha=0.75, linewidth=0.5)
-    hdlA2 = axs.annotate('', xy=(all_meanSDur[1], -2 * (pow(10, -6))), xytext=(0, -2 * (pow(10, -6))),
-                 arrowprops=dict(facecolor='green', width=2, headwidth=5), label='Mean FB duration(ms): '+condnames[1])
-    styleA2 = ArrowStyle('<->', head_length=2, head_width=1.5)
-    arrow2 = FancyArrowPatch((all_stdSDur_high[1], -2.5 * (pow(10, -6))), (all_stdSDur_low[1], -2.5 * (pow(10, -6))), arrowstyle=styleA2,
-                             color='green', label='Mean and STD of FB length(ms): '+condnames[1])
+    axs.plot(times, y2, 'black', label=condnames[1])
+    axs.plot(times, upper2[ecurr, :], 'black')
+    axs.plot(times, lower2[ecurr, :], 'black')
+    axs.fill_between(times, lower2[ecurr, :], upper2[ecurr, :], facecolor='dimgray', alpha=0.75, linewidth=0.5)
+    styleA2 = ArrowStyle('simple', head_length=5, head_width=4, tail_width=1)
+    arrow2 = FancyArrowPatch((0, -2.5),(all_meanSDur[1]*1000, -2.5),  arrowstyle=styleA2,
+                              color='dimgray', label='Mean feedback duration (msec): '+condnames[1])
     axs.add_patch(arrow2)
 
-    axs.axvline(x=0, c="black")
-    axs.axhline(y=0, c="black")
-    axs.set_title(channoms[ecurr], fontsize=10)
+    axs.axvline(x=0, c="black", linestyle="-", alpha=0.85)
+    axs.axhline(y=0, c="black", linestyle="-", alpha=0.85)
+    axs.set_title(channoms[ecurr], fontsize=10, y=1.0, pad=-10)
     axs.invert_yaxis()
     axs.set_frame_on(0)
-    axs.set_ylim(bottom=6 * (pow(10, -6)), top=-6 * (pow(10, -6)))
-    axs.set_xticks(np.arange(-0.25, 0.85, 0.2))
-    axs.tick_params(axis='x', colors='white', labelsize=8)
-    axs.set_ylabel(r"$\mu$V", fontsize=9)
+    axs.set_ylim(bottom=6, top=-8)
+    axs.set_yticks(np.arange(6, -8,-2))
+    axs.set_xticks(np.arange(-250, 850, 200))
+    axs.tick_params(axis='both', colors='white', labelsize=8)
+    #axs.annotate(r"$\mu$V", xy=(-100, -5), xycoords='data', fontsize=8)
+    axs.tick_params(axis='y', colors='black', labelsize=8)
+
+    addmsec = np.arange(2, 17, 3)
+    addmV   = np.arange(0, 15, 3)
+
+    ## Highlight current axes if stats have revealed a significant difference.
+    ChannelIndx = [EInd for EInd, ECurr in enumerate(chanList) if ECurr == channoms[ecurr]]
+    statCurr = statDF.iloc[:, ChannelIndx[0]]
+    statCurrL = statCurr.values.tolist()
+    #check for nan here and calculate the current length.
+    sdiff = np.diff(statCurrL)
+    tstep = sdiff[0]
+    DiffIdx = [dIdx for dIdx, tdiff in enumerate(list(sdiff)) if tdiff>tstep*2]
+    print(DiffIdx)
+    print(len(statCurrL))
+    tcount = 0
+    for tpnt in DiffIdx:
+         t2highlight = statCurrL[tcount:tpnt]
+         tcount = tpnt+1
+         axs.axvspan(t2highlight[0], t2highlight[-1], color="gray", alpha=0.3)
+    t2highlight = statCurrL[tcount:-1]
+    axs.axvspan(t2highlight[0], t2highlight[-1], color="gray", alpha=0.3)
+
 
     if counter == 0:
-        axs.legend(loc='upper left', bbox_to_anchor=(x0, y0, width, height), fontsize='small', frameon=False)
+        axs.legend(loc='upper left', bbox_to_anchor=(x0, y0, width, height), fontsize='medium', frameon=False)
+        img2load = makeEmptyGraph(times, EvokedAll_cond[len(eindx), :, 1], dir_base)
 
+    if counter in addmV:
+        axs.set_ylabel(r'$\mu$V', fontsize=9)
 
     if counter >= len(roi) - 3:
         axs.tick_params(axis='x', colors='black', labelsize=8)
-        axs.set_xlabel('time (seconds)', fontsize=9)
+        axs.set_xlabel('time (msec)', fontsize=9)
 
     counter += 1
 
-curr_ax = []
 
+curr_ax = []
 
 def onclick(event):
     if not event.inaxes:
